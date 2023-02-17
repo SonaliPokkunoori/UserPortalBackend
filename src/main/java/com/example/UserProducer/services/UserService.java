26782407fbfd1967b06c111dc528bc46821a1916
sonalipokkunoori <sonalipokkunoori@gmail.com>
2023-02-17 16:45:01 +0530

    Forgot Password change

--- src/main/java/com/example/UserProducer/services/UserService.java
+++ src/main/java/com/example/UserProducer/services/UserService.java
@@ -19,21 +19,21 @@ import java.util.Optional;
 public class UserService {
 
     @Autowired
-    KafkaTemplate<String,Object> kafkaTemplate;
+    KafkaTemplate<String, Object> kafkaTemplate;
 
     @Autowired
     UserRepository userRepository;
 
     @CachePut(value = "userCache")
-    public User addUserDetails(Object userDTO){
-        kafkaTemplate.send("userTopic",userDTO);
+    public User addUserDetails(Object userDTO) {
+        kafkaTemplate.send("userTopic", userDTO);
         System.out.println("Data sent to kafka");
 
-        User user=new User();
-        BeanUtils.copyProperties(userDTO,user);
+        User user = new User();
+        BeanUtils.copyProperties(userDTO, user);
         User user1 = getUserDetailsByName(user.getUserName());
         boolean checkUserName = userRepository.existsByUserName(user.getUserName());
-        if(checkUserName){
+        if (checkUserName) {
             System.out.println("Username already exists!");
             return new User();
         }
@@ -43,49 +43,47 @@ public class UserService {
     }
 
     @Cacheable(value = "userCache")
-    public User getUserDetails(String userId){
-        Optional<User> user=userRepository.findById(userId);
-        User user1=new User();
-        if(user.isPresent()){
-            user1=user.get();
+    public User getUserDetails(String userId) {
+        Optional<User> user = userRepository.findById(userId);
+        User user1 = new User();
+        if (user.isPresent()) {
+            user1 = user.get();
         }
         return user1;
     }
 
-    public User getUserDetailsByName(String userName){
-        Optional<User> user=userRepository.findByUserName(userName);
-        User user1=new User();
-        if(user.isPresent()){
-            user1=user.get();
+    public User getUserDetailsByName(String userName) {
+        Optional<User> user = userRepository.findByUserName(userName);
+        User user1 = new User();
+        if (user.isPresent()) {
+            user1 = user.get();
         }
         return user1;
     }
 
     @CacheEvict(value = "userCache")
-    public void deleteUserById(String userId){
+    public void deleteUserById(String userId) {
         userRepository.deleteById(userId);
         System.out.println("Successfully Deleted!");
     }
 
-    public StatusDTO loginStatus(LoginDTO loginDTO){
-        Optional<User> user=userRepository.findByUserName(loginDTO.getUserName());
-        StatusDTO statusDTO=new StatusDTO();
-        if(user.isPresent()) {
+    public StatusDTO loginStatus(LoginDTO loginDTO) {
+        Optional<User> user = userRepository.findByUserName(loginDTO.getUserName());
+        StatusDTO statusDTO = new StatusDTO();
+        if (user.isPresent()) {
             User user1 = user.get();
-            if(user1.getPassword().equals(loginDTO.getPassword())){
+            if (user1.getPassword().equals(loginDTO.getPassword())) {
                 statusDTO.setIsvalid(true);
                 statusDTO.setUserId(user1.userId);
                 user1.setIsvalid(true);
                 userRepository.save(user1);
                 return statusDTO;
-            }
-            else {
+            } else {
                 user1.setIsvalid(false);
                 statusDTO.setIsvalid(false);
             }
             userRepository.save(user1);
-        }
-        else{
+        } else {
             System.out.println("No User");
             return new StatusDTO();
         }
@@ -93,18 +91,18 @@ public class UserService {
         return statusDTO;
     }
 
-    public Boolean logoutStatus(String userId){
-        Optional<User> userOptional=userRepository.findById(userId);
-        User user=new User();
-        if(userOptional.isPresent()){
-            user=userOptional.get();
+    public Boolean logoutStatus(String userId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        User user = new User();
+        if (userOptional.isPresent()) {
+            user = userOptional.get();
         }
 //        else {
 //            System.out.println("No user");
 //            return null;
 //        }
         System.out.println(user);
-        if(user.getIsvalid() == true){
+        if (user.getIsvalid() == true) {
             user.setIsvalid(false);
             userRepository.save(user);
             return true;
@@ -113,13 +111,12 @@ public class UserService {
     }
 
     @CacheEvict(value = "userCache")
-    public User editProfile(User user){
+    public User editProfile(User user) {
         Optional<User> user1 = userRepository.findById(user.getUserId());
         User user2 = new User();
-        if(user1.isPresent()){
+        if (user1.isPresent()) {
             user2 = user1.get();
-        }
-        else{
+        } else {
             System.out.println("No user");
             return new User();
         }
@@ -130,25 +127,9 @@ public class UserService {
 //        user2.setMaritalStatus(user2.getMaritalStatus());
 //        user2.setEmail(user.getEmail());
 //        user2.setDob(user.getDob());
-        BeanUtils.copyProperties(user,user2);
+        BeanUtils.copyProperties(user, user2);
         userRepository.save(user2);
         System.out.println("Data Updated!!");
         return user2;
-    }
-
-    public User forgotPassword(String userId,String password){
-        Optional<User> user = userRepository.findById(userId);
-        User user1 = new User();
-        if(user.isPresent()){
-            user1=user.get();
-        }
-        else{
-            System.out.println("No User");
-            return new User();
-        }
-        user1.setPassword(password);
-        System.out.println("Updated Password!!");
-        userRepository.save(user1);
-        return user1;
     }
 }
